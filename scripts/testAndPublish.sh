ee79a3898b0b5f075281a89fd2aab09c22a62ace
Cedric Martel <cedricmtic@hotmail.com>
2016-11-01 14:26:18 -0400

    revert

--- scripts/testAndPublish.sh
+++ scripts/testAndPublish.sh
@@ -5,6 +5,8 @@ dotnet restore
 
 dotnet test ./appweb.unittests
 
-rm -rf /root/publish/*
+#rm -rf /root/publish/*
 
-dotnet publish appweb --configuration release -o /root/publish
+dotnet publish appweb -o /root/publish
+
+#dotnet publish appweb --configuration release -o /root/publish
